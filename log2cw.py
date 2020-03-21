3a552170d698368497bc93f4e8aad79e7a797b38
pydemo <36740585+pydemo@users.noreply.github.com>
2020-03-21 18:48:20 -0400

    Update log2cw.py

--- log2cw.py
+++ log2cw.py
@@ -3,25 +3,49 @@
 import boto3
 import time
 
-
-logs = boto3.client('logs', region_name='us-east-1')
-
 LOG_GROUP='TUTORIAL-DEV2'
 LOG_STREAM='stream1'
 
-logs.create_log_group(logGroupName=LOG_GROUP)
-logs.create_log_stream(logGroupName=LOG_GROUP, logStreamName=LOG_STREAM)
+
+	
+def create_log(client):
 
 
-timestamp = int(round(time.time() * 1000))
 
-response = logs.put_log_events(
-	logGroupName=LOG_GROUP,
-	logStreamName=LOG_STREAM,
-	logEvents=[
-		{
-			'timestamp': timestamp,
-			'message': time.strftime('%Y-%m-%d %H:%M:%S')+'\tHello world, here is our first log message!'
-		}
-	]
-)
+
+
+	client.create_log_group(logGroupName=LOG_GROUP)
+	client.create_log_stream(logGroupName=LOG_GROUP, logStreamName=LOG_STREAM)
+
+
+	timestamp = int(round(time.time() * 1000))
+
+	response = client.put_log_events(
+		logGroupName=LOG_GROUP,
+		logStreamName=LOG_STREAM,
+		logEvents=[
+			{
+				'timestamp': timestamp,
+				'message': time.strftime('%Y-%m-%d %H:%M:%S')+'\tHello world, here is our first log message!'
+			}
+		]
+	)
+
+
+def get_log_events(client, log_group):
+	"""List the first 10000 log events from a CloudWatch group.
+
+	:param log_group: Name of the CloudWatch log group.
+
+	"""
+
+	resp = client.filter_log_events(logGroupName=log_group, limit=10000)
+	return resp['events']
+
+
+if __name__ == '__main__':
+	client = boto3.client('logs', region_name='us-east-1')
+	for event in get_log_events(client, LOG_GROUP):
+		print(event)
+		
+		
