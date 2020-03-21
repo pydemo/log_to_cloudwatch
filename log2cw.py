92892295e2fbd8129bd27706d2eccccc949e75a7
pydemo <36740585+pydemo@users.noreply.github.com>
2020-03-21 17:13:08 -0400

    Create log2cw.py

---
+++ log2cw.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+
+import boto3
+import time
+
+
+logs = boto3.client('logs', region_name='us-east-1')
+
+LOG_GROUP='TUTORIAL-DEV2'
+LOG_STREAM='stream1'
+
+logs.create_log_group(logGroupName=LOG_GROUP)
+logs.create_log_stream(logGroupName=LOG_GROUP, logStreamName=LOG_STREAM)
+
+
+timestamp = int(round(time.time() * 1000))
+
+response = logs.put_log_events(
+	logGroupName=LOG_GROUP,
+	logStreamName=LOG_STREAM,
+	logEvents=[
+		{
+			'timestamp': timestamp,
+			'message': time.strftime('%Y-%m-%d %H:%M:%S')+'\tHello world, here is our first log message!'
+		}
+	]
+)
